7e97d04bb7152a2787b80419e449ae80f08dbd0e
秋山翔 <35907066+akiyamasho@users.noreply.github.com>
2021-08-06 11:48:49 +0900

    MAINT: use step in pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,13 +1,11 @@
 import kfp.dsl as dsl
+from steps.s01_add_inputs import (
+    add_inputs_step_generator,
+)  # we need the generator, not the function
 
 # NOTE: You can copy-paste this pipeline as a template! :D
-
 @dsl.pipeline(
-    name="Tutorial pipeline",
-    description="Add the inputs then multiple by 4"    
+    name="Tutorial pipeline", description="Add the inputs then multiple by 4"
 )
-def basic_pipeline(
-    x: int,
-    y: int
-):
-    pass
+def basic_pipeline(x: int, y: int):
+    s01_add_inputs_step = add_inputs_step_generator(x=x, y=y)
